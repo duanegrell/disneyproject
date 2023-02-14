fe1184520a0e27eecf4518b88b78a76f38653f9e
Duane Grell <duanegrell@gmail.com>
2023-02-14 16:34:47 -0500

    added basic switch

--- src/Components/Header.js
+++ src/Components/Header.js
@@ -1,11 +1,34 @@
 import React from "react";
 import disney from "../assets/disney.png";
+import { Link, NavLink } from "react-router-dom";
 
 function Header() {
     return (
-    <div>
-        <img src={disney} alt="DisneyWorld" id="main-header" />
-    </div>
+    <header>
+        <div>
+            <img src={disney} alt="DisneyWorld" id="main-header" />
+        </div>
+        <span>
+        <h3>
+            <Link to="/">
+                Home
+            </Link>
+        </h3>
+
+        <h3>
+            <Link to="/rides">
+                Rides
+            </Link>
+        </h3>
+
+        <h3>
+            <Link to="/filter">
+                Filter
+            </Link>
+        </h3>
+        </span>
+
+    </header>
     );
 };
 
