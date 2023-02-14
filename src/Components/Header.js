58770b65749687ebbc0b510337529b008d0cc40c
Duane Grell <duanegrell@gmail.com>
2023-02-14 16:46:51 -0500

    added NavBar Component

--- src/Components/Header.js
+++ src/Components/Header.js
@@ -4,31 +4,9 @@ import { Link, NavLink } from "react-router-dom";
 
 function Header() {
     return (
-    <header>
         <div>
             <img src={disney} alt="DisneyWorld" id="main-header" />
         </div>
-        <span>
-        <h3>
-            <Link to="/">
-                Home
-            </Link>
-        </h3>
-
-        <h3>
-            <Link to="/rides">
-                Rides
-            </Link>
-        </h3>
-
-        <h3>
-            <Link to="/filter">
-                Filter
-            </Link>
-        </h3>
-        </span>
-
-    </header>
     );
 };
 
